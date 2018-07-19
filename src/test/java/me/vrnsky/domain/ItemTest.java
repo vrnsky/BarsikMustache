abebf20139b24ca61105e390acc05fd2a1b7a844
Yegor Voronyansky <vrnsky@protonmail.ch>
2018-07-19 21:58:32 +0400

    first unit test for item class, which allow us to check that class extendable

--- src/test/java/me/vrnsky/domain/ItemTest.java
+++ src/test/java/me/vrnsky/domain/ItemTest.java
@@ -1,5 +1,58 @@
-import static org.junit.Assert.*;
+package me.vrnsky.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ItemTest {
+
+    /**
+     * System under test.
+     */
+    private Item item;
+
+    /**
+     * Before each test cases create a new item.
+     */
+    @Before
+    public void setUp() {
+        this.item = new Item(1, "Create a new page");
+    }
+
+    /**
+     * When call get method should check that correct id.
+     */
+    @Test
+    public void whenGetIdShouldCheckThatIdCorrect() {
+        assertThat(this.item.getId(), is(1));
+    }
+
+    /**
+     * When update id should check that updated.
+     */
+    @Test
+    public void whenUpdateIdShouldCheckThatIdSafe() {
+        this.item.setId(2);
+        assertThat(this.item.getId(), is(2));
+    }
+
+    /**
+     * When get description should check that is correct.
+     */
+    @Test
+    public void whenGetDescriptionShouldCheckThatIsCorrect() {
+        assertThat(this.item.getDescription(), is("Create a new page"));
+    }
+
+    /**
+     * When update description should check that updated.
+     */
+    @Test
+    public void whenUpdateDescriptionShouldCheckThatSafeIt() {
+        this.item.setDescription("Write code");
+        assertThat(this.item.getDescription(), is("Write code"));
+    }
 
 }
