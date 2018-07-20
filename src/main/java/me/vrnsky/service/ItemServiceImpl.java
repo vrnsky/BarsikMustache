319cccc88d40c349114719e26ca64d500bdc584b
Yegor Voronyansky <vrnsky@protonmail.ch>
2018-07-21 00:00:42 +0400

    Item service which use repository to interact with database, related to #2

--- src/main/java/me/vrnsky/service/ItemServiceImpl.java
+++ src/main/java/me/vrnsky/service/ItemServiceImpl.java
@@ -1,6 +1,28 @@
 package me.vrnsky.service;
 
+import me.vrnsky.domain.Item;
+import me.vrnsky.persistence.interfaces.ItemRepository;
 import me.vrnsky.service.interfaces.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ItemServiceImpl implements ItemService {
+
+    private ItemRepository repository;
+
+    @Autowired
+    public ItemServiceImpl(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(Item item) {
+        this.repository.add(item);
+    }
+
+    public List<Item> all() {
+        return this.repository.all();
+    }
 }
--- src/main/java/me/vrnsky/service/interfaces/ItemService.java
+++ src/main/java/me/vrnsky/service/interfaces/ItemService.java
@@ -1,4 +1,12 @@
 package me.vrnsky.service.interfaces;
 
+import me.vrnsky.domain.Item;
+
+import java.util.List;
+
 public interface ItemService {
+
+    void add(Item item);
+
+    List<Item> all();
 }
